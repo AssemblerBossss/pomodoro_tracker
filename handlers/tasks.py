dc089f01d723b61e0730cdc3917697902e6bb298
AssemblerBossss <nazarovmisa39@mail.ru>
2025-08-11 14:08:12 +0300

    примитивное кэширование запросов тудушек

--- handlers/tasks.py
+++ handlers/tasks.py
@@ -1,3 +1,4 @@
+from time import sleep
 from typing import Annotated
 from uuid import UUID
 from fastapi import APIRouter, status, Depends
@@ -14,21 +15,27 @@ async def get_tasks(
     task_repository: Annotated[TaskRepository, Depends(get_tasks_repository)],
     task_cache: Annotated[TaskCache, Depends(get_cache_tasks_repository)]
 ):
-    if tasks := task_cache.get_tasks():
-        return tasks
-    else:
-        tasks = task_repository.get_all_tasks()
+    cached_tasks = await task_cache.get_tasks()
+
+    if cached_tasks:
+        return cached_tasks
+
+    tasks = task_repository.get_all_tasks()
+    sleep(10)
+    if tasks:
         tasks_schema = [TaskResponse.model_validate(task) for task in tasks]
         task_cache.set_tasks(tasks_schema)
-        return tasks
+    return tasks
 
 
 @router.post("/", response_model=TaskResponse)
 async def create_task(
     task: TaskCreate,
     task_repository: Annotated[TaskRepository, Depends(get_tasks_repository)],
+    task_cache: Annotated[TaskCache, Depends(get_cache_tasks_repository)]
 ):
     response_task = task_repository.create_task(task)
+    task_cache.add_task(TaskResponse.model_validate(response_task))
     return response_task
 
 
@@ -44,8 +51,10 @@ async def update_task(
 async def delete_task(
     task_id: UUID,
     task_repository: Annotated[TaskRepository, Depends(get_tasks_repository)],
+    task_cache: Annotated[TaskCache, Depends(get_cache_tasks_repository)]
 ):
     task_repository.delete_task(task_id)
+    task_cache.invalidate_cache()
 
 
 #
--- repository/cache_tasks.py
+++ repository/cache_tasks.py
@@ -8,21 +8,34 @@ class TaskCache:
 
     def __init__(self, redis: Redis):
         self.redis = redis
+        self.cache_key = "tasks_list"
 
     def get_tasks(self) -> list[TaskResponse]:
         with self.redis as redis:
             # Получаем все элементы списка
-            tasks_data = redis.lrange('tasks', 0, -1)
+            tasks_data = redis.lrange(self.cache_key , 0, -1)
             # Декодируем каждый элемент и парсим JSON
             return [
                 TaskResponse.model_validate(json.loads(task.decode('utf-8')))
                 for task in tasks_data
-            ]
+            ] if tasks_data else []
 
     def set_tasks(self, tasks: list[TaskResponse]) -> None:
         tasks_json = [task.model_dump_json() for task in tasks]
         with self.redis as redis:
             # Удаляем старый список (опционально)
-            redis.delete('tasks')
+            redis.delete(self.cache_key)
             # Добавляем новые задачи
-            redis.lpush("tasks", *tasks_json)
+            redis.rpush(self.cache_key , *tasks_json)
+
+            redis.expire(self.cache_key, 300)
+
+    def add_task(self, task: TaskResponse) -> None:
+        """Добавление одной задачи в кэш"""
+        with self.redis as redis:
+            redis.rpush(self.cache_key, task.model_dump_json())
+
+    def invalidate_cache(self) -> None:
+        """Очистка кэша"""
+        with self.redis as redis:
+            redis.delete(self.cache_key)
